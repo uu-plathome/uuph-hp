a84553df9f3c8fe46a3b5593e136a9c3f7a16b97
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-02-18 22:58:10 +0900

    fix site-title

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>UU-PlatHome | 宇大生必須のプラットフォーム</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

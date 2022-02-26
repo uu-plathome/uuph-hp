3f7ee931fbf9c3b28b60a4ad9f9b4d32e27e1f52
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2022-02-27 03:35:05 +0900

    feat create-contact-page

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -20,14 +20,15 @@ export default class MyDocument extends Document {
           {/** M plus 2p from adobe font */}
           <script dangerouslySetInnerHTML={{
             __html: `
-          (function(d) {
-          var config={
-            kitId: 'uos7nye' ,
-            scriptTimeout: 3000,
-            async: true
-          },
-          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+"wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded" )return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);
-        ` }} />
+              (function(d) {
+              var config={
+                kitId: 'uos7nye' ,
+                scriptTimeout: 3000,
+                async: true
+              },
+              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+"wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded" )return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);
+            `
+          }} />
 
           <link rel="icon" href="/favicon.png" />
 
@@ -39,20 +40,21 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', 'G-XR6X17WHFF', {
-                                page_path: window.location.pathname,
-                            });`,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-XR6X17WHFF', {
+                    page_path: window.location.pathname,
+                });`,
             }}
           />
-
         </Head>
+
         <body>
           <Main />
           <NextScript />
         </body>
+
       </Html>
     )
   }
